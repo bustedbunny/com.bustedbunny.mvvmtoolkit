0e6efd34b6e3501a7cc20ed25ced3a105d93c381
bustedbunny <buster1998@Mail.ru>
2023-02-07 10:34:47 +0300

    LocalizedString binding is now async to support non-synchronous loading

--- Runtime/Binding/Localization/LocalizedTextBinding.cs
+++ Runtime/Binding/Localization/LocalizedTextBinding.cs
@@ -1,6 +1,10 @@
 using System;
 using System.ComponentModel;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
 using UnityEngine.Localization.Tables;
 using UnityEngine.UIElements;
 
@@ -10,7 +14,8 @@ namespace MVVMToolkit.Binding.Localization
     {
         private readonly TextElement _element;
         private readonly Action<VisualElement, string> _operation;
-        private readonly LocalizedStringBinding _stringBinding;
+        private LocalizedStringBinding _stringBinding;
+        private readonly CancellationTokenSource _cts;
 
         public LocalizedTextBinding(TextElement element, INotifyPropertyChanged binding, string key,
             LocalizedStringTable[] tables,
@@ -19,21 +24,44 @@ namespace MVVMToolkit.Binding.Localization
             _operation = operation;
             _element = element;
 
-            var table = GetMatchingTable(tables, key);
+            _cts = new();
+            BindAsync(binding, key, tables, _cts.Token).Forget();
+        }
+
+        // Binding operation has to be async in case LocalizationSettings are not loaded yet
+        private async UniTaskVoid BindAsync(INotifyPropertyChanged binding, string key,
+            LocalizedStringTable[] tables, CancellationToken ct)
+        {
+            var operation = LocalizationSettings.InitializationOperation;
+            while (!operation.IsDone)
+            {
+                if (ct.IsCancellationRequested) return;
+                await UniTask.Yield();
+            }
+
+            var table = await GetMatchingTable(tables, key);
 
             LocalizedString s = new(table, key);
 
             _stringBinding = new(s, binding);
 
+
             s.StringChanged += StringChanged;
         }
 
-        private TableReference GetMatchingTable(LocalizedStringTable[] tables, string key)
+        // Tables might not be loaded as well, so we have to await them loading too
+        private async UniTask<TableReference> GetMatchingTable(LocalizedStringTable[] tables, string key)
         {
             foreach (var table in tables)
             {
+                var handle = table.CurrentLoadingOperationHandle;
+                if (!handle.IsValid()) handle = table.GetTableAsync();
+
+                await handle;
+
                 if (table.TableReference.SharedTableData.Contains(key))
                 {
+                    table.TableChanged += TableChanged;
                     return table.TableReference;
                 }
             }
@@ -41,9 +69,23 @@ namespace MVVMToolkit.Binding.Localization
             throw new BindingException($"No key: {key} found in provided tables.");
         }
 
-        private void StringChanged(string value) => _operation(_element, value);
+        private void TableChanged(StringTable value)
+        {
+            Debug.Log("TableChanged");
+        }
+
+        private void StringChanged(string value)
+        {
+            Debug.Log("stringChanged");
+            _operation(_element, value);
+        }
 
 
-        public void Dispose() => _stringBinding.Dispose();
+        public void Dispose()
+        {
+            _cts.Cancel();
+            _cts.Dispose();
+            _stringBinding?.Dispose();
+        }
     }
 }
