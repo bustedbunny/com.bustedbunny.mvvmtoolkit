d68d5a2066866418c065e846cf52936530607b6e
bustedbunny <buster1998@mail.ru>
2023-02-21 00:22:31 +0300

    MultiSolver now supports unmanaged types, but asset binding now uses double cast in order to bind

--- Runtime/Binding/Utility/Generics/MultiSolver.cs
+++ Runtime/Binding/Utility/Generics/MultiSolver.cs
@@ -14,7 +14,7 @@ namespace MVVMToolkit.Binding.Generics
         public Action Solve(MethodInfo get, object source, MethodInfo set, object target);
     }
 
-    public abstract class MultiSolver<TGet, TSet> : IMultiSolver where TGet : Object
+    public abstract class MultiSolver<TGet, TSet> : IMultiSolver
     {
         public Type GetterType => typeof(TGet);
         public Type SetterType => typeof(TSet);
@@ -24,7 +24,7 @@ namespace MVVMToolkit.Binding.Generics
         public Action<Object> ResolveAssetSetter(PropertyInfo setProp, object target)
         {
             var set = HelpersGenerics.Set<TSet>(setProp.GetSetMethod(true), target);
-            return value => set(Convert(Unsafe.As<TGet>(value)));
+            return value => set(Convert((TGet)(object)value));
         }
 
         public Action Solve(MethodInfo get, object source, MethodInfo set, object target)
