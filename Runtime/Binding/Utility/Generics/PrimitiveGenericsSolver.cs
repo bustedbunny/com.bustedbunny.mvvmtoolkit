1affb6d46ec65da82f17c663f68acdcfdfc91f9a
bustedbunny <buster1998@mail.ru>
2023-03-05 20:16:03 +0300

    Added preserve tag on all built-in solvers

--- Runtime/Binding/Utility/Generics/PrimitiveGenericsSolver.cs
+++ Runtime/Binding/Utility/Generics/PrimitiveGenericsSolver.cs
@@ -1,16 +1,25 @@
+using UnityEngine.Scripting;
+
 namespace MVVMToolkit.Binding.Generics
 {
+    [Preserve]
     public class IntSolver : SingleSolver<int> { }
 
+    [Preserve]
     public class UintSolver : SingleSolver<uint> { }
 
+    [Preserve]
     public class ByteSolver : SingleSolver<byte> { }
 
+    [Preserve]
     public class FloatSolver : SingleSolver<float> { }
 
+    [Preserve]
     public class DoubleSolver : SingleSolver<double> { }
 
+    [Preserve]
     public class StringSolver : SingleSolver<string> { }
 
+    [Preserve]
     public class BoolSolver : SingleSolver<bool> { }
 }
