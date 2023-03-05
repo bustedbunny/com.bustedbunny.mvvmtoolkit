07077cdbb5fbb90cc91bcc7ba92c4693f30c3034
bustedbunny <buster1998@mail.ru>
2023-03-05 20:14:47 +0300

    TablePostprocessor is now obsolete and left only to cleanup for itself.

--- Runtime/Binding/Localization/TablePostprocessor.cs
+++ Runtime/Binding/Localization/TablePostprocessor.cs
@@ -5,49 +5,22 @@ using UnityEngine.Localization.Tables;
 
 namespace MVVMToolkit.Binding.Localization
 {
-    public delegate void TableLoadedEvent();
-
     /// <summary>
-    /// Table postprocessor provides static event for localization binding to analyze tables before values are evaluated
-    /// and errors about unresolved variables is thrown
+    /// Table postprocessor is obsolete and is only cleaning up for itself.
+    /// Will be removed eventually
     /// </summary>
     [Serializable]
     internal class TablePostprocessor : ITablePostprocessor
     {
-        private static ITablePostprocessor _previousValue;
-
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         private static void Register()
         {
-            _previousValue = LocalizationSettings.StringDatabase.TablePostprocessor;
-            LocalizationSettings.StringDatabase.TablePostprocessor = new TablePostprocessor();
-
-            LocalizationSettings.StringDatabase.SmartFormatter.Parser.AddOperators("#");
-        }
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void ReloadStatic()
-        {
-            if (OnTableLoaded is not null)
+            if (LocalizationSettings.StringDatabase.TablePostprocessor is TablePostprocessor)
             {
-                var kek = OnTableLoaded.GetInvocationList();
-                foreach (var del in kek)
-                {
-                    OnTableLoaded -= (TableLoadedEvent)del;
-                }
+                LocalizationSettings.StringDatabase.TablePostprocessor = null;
             }
         }
 
-        public static event TableLoadedEvent OnTableLoaded;
-
-        public void PostprocessTable(LocalizationTable table)
-        {
-            OnTableLoaded?.Invoke();
-
-            if (_previousValue is not null && _previousValue is not TablePostprocessor)
-            {
-                _previousValue?.PostprocessTable(table);
-            }
-        }
+        public void PostprocessTable(LocalizationTable table) { }
     }
 }
