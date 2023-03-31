6e65cedb165d893778d34d30489beac21aa2cf3a
bustedbunny <30902981+bustedbunny@users.noreply.github.com>
2023-03-31 10:20:21 +0300

    Added thread-safe version for Burst Wrapper.

--- Runtime/Messaging/BurstWrapper/MessengerWrapper.cs
+++ Runtime/Messaging/BurstWrapper/MessengerWrapper.cs
@@ -1,6 +1,8 @@
+using System.Threading;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine.Scripting;
 
 namespace MVVMToolkit.Messaging
 {
@@ -15,6 +17,29 @@ namespace MVVMToolkit.Messaging
 
             data.AddRangeNoResize(UnsafeUtility.AddressOf(ref hash), 8);
             data.AddRangeNoResize(UnsafeUtility.AddressOf(ref message), size);
+        }
+
+        [Preserve]
+        public Parallel AsParallel => new()
+        {
+            data = data.AsParallelWriter()
+        };
+
+        public struct Parallel
+        {
+            internal NativeList<byte>.ParallelWriter data;
+
+            public unsafe void Send<T>(T message) where T : unmanaged
+            {
+                var size = sizeof(T);
+                var hash = BurstRuntime.GetHashCode64<T>();
+
+                var length = 8 + size;
+                var idx = Interlocked.Add(ref data.ListData->m_length, length) - length;
+                var ptr = (byte*)data.Ptr + idx;
+                UnsafeUtility.MemCpy(ptr, UnsafeUtility.AddressOf(ref hash), 8);
+                UnsafeUtility.MemCpy(ptr + 8, UnsafeUtility.AddressOf(ref message), size);
+            }
         }
     }
 }
