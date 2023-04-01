8209c09395a0afde9f8dddf7f51e4c022bb8893a
bustedbunny <30902981+bustedbunny@users.noreply.github.com>
2023-04-01 13:28:02 +0300

    Added generic type map templates

---
+++ Runtime/Binding/Utility/Generics/TypeMap/StringInstanceMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using MVVMToolkit.TypeSerialization;
+
+namespace MVVMToolkit.Binding.Generics
+{
+    public abstract class StringInstanceMap<T>
+    {
+        private static readonly Dictionary<string, Type> Map;
+
+        static StringInstanceMap()
+        {
+            Map = new();
+            foreach (var type in TypeUtility.GetTypes(typeof(T)))
+            {
+                Map.Add(type.Name, type);
+            }
+        }
+
+        public static T GetInstance(string key) => (T)Activator.CreateInstance(Map[key]);
+    }
+}
---
+++ Runtime/Binding/Utility/Generics/TypeMap/StringInstanceMapShared.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using MVVMToolkit.TypeSerialization;
+
+namespace MVVMToolkit.Binding.Generics
+{
+    public abstract class StringInstanceMapShared<T>
+    {
+        private static readonly Dictionary<string, T> Map;
+
+        static StringInstanceMapShared()
+        {
+            Map = new();
+            foreach (var type in TypeUtility.GetTypes(typeof(T)))
+            {
+                Map.Add(type.Name, (T)Activator.CreateInstance(type));
+            }
+        }
+
+        public static T GetInstance(string key) => Map[key];
+    }
+}
