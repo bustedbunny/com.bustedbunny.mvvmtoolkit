e2def8fd3b47309aec4e3d8efa53b7105a6641be
bustedbunny <30902981+bustedbunny@users.noreply.github.com>
2023-04-17 21:01:33 +0300

    Added CommandUtility as high level API for obtaining commands from observable objects

---
+++ Runtime/Binding/Utility/CommandUtility.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace MVVMToolkit.Binding
+{
+    public static class CommandUtility
+    {
+        public static void FormatCommandPath(ref string prompt)
+        {
+            const string suffix = "Command";
+            if (!prompt.EndsWith(suffix))
+            {
+                prompt += suffix;
+            }
+
+            const string prefix = "On";
+            if (prompt.StartsWith(prefix))
+            {
+                prompt = prompt[2..];
+            }
+        }
+
+        public static IRelayCommand GetCommand(object source, string prompt)
+        {
+            FormatCommandPath(ref prompt);
+            var property = PropertyUtility.GetGetProperty(source, prompt);
+            return property.GetValue(source) as IRelayCommand;
+        }
+    }
+}
